4651250e2364cf04c4a9133b15d9d70ce39ca26c
Jadon Fowler <jadonflower@gmail.com>
2016-07-03 15:28:51 -0700

    Output Fields for JVM and start on Constructor
    Fields are created by the FieldVisitor and the constructor is created
    by the MV.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- src/xyz/jadonfowler/lang/codegen/JVMBackend.java
+++ src/xyz/jadonfowler/lang/codegen/JVMBackend.java
@@ -6,9 +6,11 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import xyz.jadonfowler.lang.ast.AbstractClassTree;
 import xyz.jadonfowler.lang.ast.LClass;
+import xyz.jadonfowler.lang.ast.LField;
 import xyz.jadonfowler.lang.ast.LMethod;
 import xyz.jadonfowler.lang.ast.LModifier;
 import xyz.jadonfowler.lang.ast.LParameter;
+import xyz.jadonfowler.lang.ast.LType;
 
 public class JVMBackend extends Backend implements Opcodes {
 
@@ -22,17 +24,41 @@ public class JVMBackend extends Backend implements Opcodes {
         FieldVisitor fv;
         MethodVisitor mv;
         {
-            cw.visit(V1_8, ACC_PUBLIC, clazz.getModule() + clazz.getName(), null, "java/lang/Object", null);
+            cw.visit(V1_8, ACC_PUBLIC, clazz.getModule() + "." + clazz.getName(), null, "java/lang/Object", null);
+            // constructor
+            StringBuffer constructorDescription = new StringBuffer();
+            constructorDescription.append("(");
+            for (LParameter parameter : clazz.getConstructor().getParameters())
+                constructorDescription.append(parameter.getType().getDescription());
+            constructorDescription.append(")V"); // constructors return void
+            mv = cw.visitMethod(ACC_PUBLIC, "<init>", constructorDescription.toString(), null, null);
+            mv.visitCode();
+            // mv.visitVarInsn(ALOAD, 0);
+            // mv.visitMethodInsn(0, "java/lang/Object", "<init>",
+            // constructorDescription.toString(), false);
+            // fields are initialized in the constructor
+            for (LField field : clazz.getFields()) {
+                if (field.getType().equals(LType.VOID))
+                    continue; // ignore void variables
+                fv = cw.visitField(0, field.getName(), field.getType().getDescription(), null, null);
+                fv.visitEnd();
+            }
+            // mv.visitMaxs(1 + fieldCount, 1 + this.variables.size() +
+            // argumentCount);
+            mv.visitInsn(RETURN);
+            mv.visitEnd();
+
+            // methods
             for (LMethod method : clazz.getMethods()) {
                 int modifiers = 0;
                 for (LModifier modifier : method.getModifiers())
                     modifiers |= modifier.getModifier();
-                StringBuffer description = new StringBuffer();
-                description.append("(");
+                StringBuffer methodDescription = new StringBuffer();
+                methodDescription.append("(");
                 for (LParameter parameter : method.getParameters())
-                    description.append(parameter.getType().getDescription());
-                description.append(")" + method.getReturnType().getDescription());
-                mv = cw.visitMethod(modifiers, method.getName(), description.toString(), null, null);
+                    methodDescription.append(parameter.getType().getDescription());
+                methodDescription.append(")" + method.getReturnType().getDescription());
+                mv = cw.visitMethod(modifiers, method.getName(), methodDescription.toString(), null, null);
                 mv.visitCode();
                 // end
                 mv.visitInsn(RETURN);
